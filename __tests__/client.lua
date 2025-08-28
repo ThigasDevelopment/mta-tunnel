092869037963c78bac757f43d1bcc184d7a36891
ThigasDevelopment <maia.thico@gmail.com>
2025-08-28 02:12:35 -0300

    :sparkles: Added logic tests on server & client files.

--- __tests__/client.lua
+++ __tests__/client.lua
@@ -1,12 +1,26 @@
 -- interface's
 local server = Tunnel.get ('APIServer');
 
-server.get (1, 2, 3):try (
-	function (...)
-		iprint ('try response: ', ...);
-	end
-):catch (
-	function (error)
-		iprint ('catch error: ', error);
-	end
+setTimer (
+	function ()
+		server.get (1, 2, 3):try (
+			function (...)
+				iprint ('on server try response: ', ...);
+			end
+		):catch (
+			function (error)
+				iprint ('on server catch error: ', error);
+			end
+		);
+	end, 1000, 1
 );
+
+local client = { };
+
+function client.get (...)
+	iprint ('client', ...);
+
+	return (math.random (1, 2) == 2), math.random (1, 1000);
+end
+
+Tunnel.bind ('APIClient', client);
--- __tests__/server.lua
+++ __tests__/server.lua
@@ -1,10 +1,27 @@
 -- interface's
+local client = Tunnel.get ('APIClient');
+
+setTimer (
+	function ()
+		client.get (1, 2, 3):try (
+			function (...)
+				iprint ('on client try response: ', ...);
+			end
+		):catch (
+			function (error)
+				iprint ('on client catch error: ', error);
+			end
+		);
+	end, 1000, 1
+);
+
 local server = { };
 
 function server.get (player, ...)
-	iprint (player, ...);
+	iprint ('server', player, ...);
 
-	return false, math.random (1, 1000);
+	return (math.random (1, 2) == 1), math.random (1, 1000);
 end
 
 Tunnel.bind ('APIServer', server);
+
